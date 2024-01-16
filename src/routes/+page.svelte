a6bcf9f0a86c1017c3972966b6a6f83b8c8c66f8
BraidaFac <facundobraida98@gmail.com>
2024-01-16 12:43:51 -0300

    table created

---
+++ src/routes/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
+    import type { Tab, TableSource } from '@skeletonlabs/skeleton';
+    let array:any[] = [];
+    let TableSource :TableSource;
+    onMount(async () => {
+        const response = await fetch('Listado.csv');
+        const data = await response.text();
+        const rows = data.split('\n').slice(2);
+        array = rows.map(row => {
+            const columns = row.split(';');
+            return {
+                codigo: columns[0],
+                descripcion: columns[1],
+                marca: columns[2],
+                tipo_articulo: columns[3],
+                clase: columns[4],
+                detalle : columns[5],
+                activo : columns[6],
+                stock_negativo: columns[7],
+                fecha_vencimiento: columns[8],
+                certificado_calidad: columns[9],
+                codigo_barra: columns[10],
+                fecha_alta: columns[11], 
+                publicado_web: columns[12],
+                destacado_web : columns[13],   
+            }
+        });
+        array=array.slice(0, 27000);
+        
+  
+    });
+    
+   $: if(array.length > 0 ){
+     tableSource : TableSource= {
+        	head: ['Codigo', 'Descripcion', 'Marca', 'Tipo Articulo', 'Clase', 'Detalle', 'Activo', 'Stock Negativo', 'Fecha Vencimiento', 'Certificado Calidad', 'Codigo Barra', 'Fecha Alta', 'Publicado Web', 'Destacado Web'],
+	// The data visibly shown in your table body UI.
+	body: tableMapperValues(array, ['codigo', 'descripcion', 'marca', 'tipo_articulo', 'clase', 'detalle', 'activo', 'stock_negativo', 'fecha_vencimiento', 'certificado_calidad', 'codigo_barra', 'fecha_alta', 'publicado_web', 'destacado_web']),
+     }
+    }
+    
+  
+</script>
+<div>
+    {#if array.length > 0}
+        <Table source={TableSource} />
+    {:else}
+        <p>Loading...</p>
+    {/if}
+</div>
